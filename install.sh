af1805bc8f252eb87d506081c68a04e9e86bb31e
Bartosz Celmer <cb1986ster@gmail.com>
2017-03-07 17:08:24 +0100

    Dodanie edytora dla zwykłych userów

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-php=php
-$php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-$php -r "if (hash_file('SHA384', 'composer-setup.php') === '55d6ead61b29c7bdee5cccfb50076874187bd9f21f65d8991d46ec5cc90518f447387fb9f76ebae1fbbacf329e583e30') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-$php composer-setup.php
-$php -r "unlink('composer-setup.php');"
-$php composer.phar install
-$php bin/console cache:clear --no-warmup
-$php bin/console cache:clear --no-warmup --env=prod
-$php bin/console doctrine:database:create
-$php bin/console doctrine:schema:update --force
-$php bin/console fos:user:create --super-admin admin bcelmer@is.umk.pl admin
-chmod -R 777 var
-chmod  777 .
-chmod  777 data.db3
+php=php ;
+$php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" ;
+$php composer-setup.php ;
+$php -r "unlink('composer-setup.php');" ;
+mv composer.phar composer ;
+$php composer.phar install ;
+$php bin/console cache:clear --no-warmup ;
+$php bin/console cache:clear --no-warmup --env=prod ;
+$php bin/console doctrine:database:create ;
+$php bin/console doctrine:schema:update --force ;
+$php bin/console fos:user:create --super-admin admin bcelmer@is.umk.pl admin ;
+$php bin/console asset:install ;
+$php bin/console assetic:dump ;
+chmod -R 777 var ;
+chmod  777 . ;
+chmod  777 data.db3 ;
